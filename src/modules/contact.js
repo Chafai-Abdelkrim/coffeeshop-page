d52ab3fe03f5f0c8ad09fdb6109de3fb9026a53c
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2022-09-13 12:29:55 +0100

    coded the contact js file

---
+++ src/modules/contact.js
@@ -0,0 +1,76 @@
+const createTitle = () => {
+    const title = document.createElement('h2');
+    title.classList.add('contact-title');
+    title.innerText = 'Contact us! ';
+
+    return title;
+}
+//function to create contact details elements
+const createContactDetails = () => {
+    const Container = document.createElement('div');
+    Container.classList.add('contact-details');
+
+    const title = document.createElement('h3');
+    title.classList.add('contact-details-title');
+    title.innerText = 'Address';
+
+    const address = document.createElement('p');
+    address.classList.add('contact-details-text');
+
+    const phoneEmail = document.createElement('p');
+    phoneEmail.classList.add('contact-details-text');
+
+    const lineBreak = () => document.createElement('br');
+
+    address.append('Av. Tifaouine', lineBreak(), 'Agadir, Morocco', lineBreak(), '9FXP+J8');
+    phoneEmail.append('Phone Number: 0528285288', lineBreak(), 'Email: O_brown@fakeemail.com');
+
+    Container.append(title);
+    Container.append(address);
+    Container.append(phoneEmail);
+
+    return Container;
+}
+
+const createSchedule = () => {
+    const Container = document.createElement('div');
+    Container.classList.add('contact-details');
+
+    const title = document.createElement('h3');
+    title.classList.add('contact-details-title');
+    title.innerText = 'Schedule';
+
+    const hours = document.createElement('p');
+    hours.classList.add('contact-details-text');
+
+    const lineBreak = () => document.createElement('br');
+
+    hours.append('Monday: 9:00am to 10:00pm', lineBreak());
+    hours.append('Tuesday: 9:00am to 10:00pm', lineBreak());
+    hours.append('Wednesday: 9:00am to 10:00pm', lineBreak());
+    hours.append('Thursday: 9:00am to 10:00pm', lineBreak());
+    hours.append('Friday: 9:00am to 10:00pm', lineBreak());
+    hours.append('Saturday: 9:00am to 10:00pm', lineBreak());
+    hours.append('Sunday: 9:00am to 10:00pm');
+
+    Container.append(title);
+    Container.append(hours);
+
+    return Container;
+}
+
+const Contact = () => {
+    const main = document.querySelector('.main');
+    main.textContent = '';
+
+    const contactContainer = document.createElement('div');
+    contactContainer.classList.add('contact-container');
+    const title = createTitle();
+    const details = createContactDetails();
+    const Schedule = createSchedule();
+
+    contactContainer.append(details, Schedule);
+    main.append(title, contactContainer);
+}
+
+export { Contact }
